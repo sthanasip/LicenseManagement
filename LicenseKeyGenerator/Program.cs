8420d3d6bf7e69f3a2b4282ec709a75fa0ac289d
Nasip Shrestha <nasipshrestha@gmail.com>
2024-06-04 14:39:56 +0545

    set page of license key generator as the default page that loads when program starts

--- LicenseKeyGenerator/Program.cs
+++ LicenseKeyGenerator/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=LicenseKeyGenerator}/{action=Index}/{id?}");
 
 app.Run();
