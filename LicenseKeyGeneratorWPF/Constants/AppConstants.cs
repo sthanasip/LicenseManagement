8bf0867b546397e2dcc623bf604b15c1b79b7138
Nasip Shrestha <nasipshrestha@gmail.com>
2024-06-06 11:23:17 +0545

    bug resolution and enhancements related to date and number of license

---
+++ LicenseKeyGeneratorWPF/Constants/AppConstants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseKeyGeneratorWPF.Constants
+{
+    public static class AppConstants
+    {
+        public const string InvalidInputType = "Invalid Input";
+        public const string InvalidDateType = "Invalid Date";
+
+        public const int MinimumNumberOfLicenses = 1;
+        public const int LengthOfDate = 8;
+        public const int IndexOfDays = 2;
+        public const int IndexAfterMonth = 5;
+        public const int MaxNumberOfDays = 31;
+        public const int MaxNumberOfMonth = 12;
+        public const int MinimumDaysAndMonth = 1;
+
+
+
+
+        public const string EncryptionKey = "HardCodedEncryptionKey123";
+
+        //public const string DateFormatPattern = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/([0-9]{4})$";
+        public const string NumberPattern = @"[^0-9]";
+    }
+}
--- LicenseKeyGeneratorWPF/MainWindow.xaml.cs
+++ LicenseKeyGeneratorWPF/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
-﻿using LicenseKeyGeneratorWPF.Helpers;
+﻿using LicenseKeyGeneratorWPF.Constants;
+using LicenseKeyGeneratorWPF.Helpers;
 using LicenseKeyGeneratorWPF.Records;
-using System;
-using System.IO;
-using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace LicenseKeyGeneratorWPF
 {
@@ -17,45 +17,122 @@ namespace LicenseKeyGeneratorWPF
             InitializeComponent();
         }
 
-        private const string encryptionKey = "HardCodedEncryptionKey123";
         private void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
             string requestKey = txtRequestKey.Text;
             string numberOfConnectionsText = txtNumberOfConnections.Text;
             string expiryDate = txtExpiryDate.Text;
+            int numberOfConnections = default;
 
-            if (!ValidateExpiryDate(expiryDate))
+            if (!ValidateForm(requestKey, numberOfConnectionsText, expiryDate, ref numberOfConnections))
             {
-                MessageBox.Show("Date must be in the format dd/MM/yyyy.", "Invalid Date", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
-            if (!int.TryParse(numberOfConnectionsText, out int numberOfConnections))
-            {
-                MessageBox.Show("Number of Connections must be a valid integer.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            string licenseCode = GenerateLicenseCode(requestKey, numberOfConnections, expiryDate);
-            txtLicenseCode.Text = licenseCode;
+            txtLicenseCode.Text = GenerateLicenseCode(requestKey, numberOfConnections, expiryDate);
         }
 
-        private bool ValidateExpiryDate(string date)
+        private bool ValidateForm(string requestKey, string numberOfConnectionsText, string expiryDate, ref int numberOfConnections)
         {
-            string pattern = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/([0-9]{4})$";
-            return Regex.IsMatch(date, pattern);
+            if (string.IsNullOrEmpty(requestKey))
+            {
+                return DisplayMessageBox("Request Key cannot be empty", AppConstants.InvalidInputType);
+            }
+
+            if (!int.TryParse(numberOfConnectionsText, out numberOfConnections) || numberOfConnections < AppConstants.MinimumNumberOfLicenses)
+            {
+                return DisplayMessageBox("Number of Connections must be a valid integer and greater than 0.", AppConstants.InvalidInputType);
+            }
+
+            if (ExpiryDateIsNotFutureDate(expiryDate)) {
+                return DisplayMessageBox("License expiry date must be later than current date", AppConstants.InvalidDateType);
+            }
+            
+            return true;
+        }
+
+        private bool DisplayMessageBox(string message, string type)
+        {
+            MessageBox.Show(message, type, MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) => e.Handled = Regex.IsMatch(e.Text, AppConstants.NumberPattern);
+
+        private void DateTextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                string text = textBox.Text.Replace("/", string.Empty); // Remove existing slashes
+                if (text.Length > AppConstants.LengthOfDate) // Limit the length to 8 characters (ddMMyyyy)
+                {
+                    text = text.Substring(0, AppConstants.LengthOfDate);
+                }
+
+                // Insert slashes after every 2 digits
+                for (int i = AppConstants.IndexOfDays; i < text.Length; i += 3)
+                {
+                    if (i <= AppConstants.IndexAfterMonth)
+                    {
+                        text = text.Insert(i, "/");
+                    }
+                }
+
+                // Validate day and month
+                if (text.Length >= AppConstants.IndexOfDays && !IsValidDay(text.Substring(0, AppConstants.IndexOfDays)))
+                {
+                    textBox.Text = text.Substring(0, 1);
+                    textBox.CaretIndex = textBox.Text.Length;
+                    return;
+                }
+
+                if (text.Length >= AppConstants.IndexAfterMonth && !IsValidMonth(text.Substring(3, 2)))
+                {
+                    textBox.Text = text.Substring(0, 4);
+                    textBox.CaretIndex = textBox.Text.Length;
+                    return;
+                }
+
+                textBox.Text = text;
+                textBox.CaretIndex = textBox.Text.Length; // Move caret to the end
+            }
+        }
+
+        private bool IsValidDay(string dayText)
+        {
+            if (int.TryParse(dayText, out int day))
+            {
+                return day >= AppConstants.MinimumDaysAndMonth && day <= AppConstants.MaxNumberOfDays;
+            }
+            return false;
+        }
+
+        private bool IsValidMonth(string monthText)
+        {
+            if (int.TryParse(monthText, out int month))
+            {
+                return month >= AppConstants.MinimumDaysAndMonth && month <= AppConstants.MaxNumberOfMonth;
+            }
+            return false;
+        }
+
+        private bool ExpiryDateIsNotFutureDate(string date)
+        {
+            var expiryDate = DateTime.ParseExact(date, "dd/MM/yyyy", null); ;
+            return expiryDate.Date < DateTime.UtcNow.AddDays(1).Date;
         }
 
         private string GenerateLicenseCode(string requestKey, int numberOfConnections, string expiryDate)
         {
             //decrypt key to get hashed value
             var key = Encoding.UTF8.GetString(Convert.FromBase64String(requestKey));
-            var decryptedKey = EncryptionService.DecryptString(key, encryptionKey);
+            var decryptedKey = EncryptionService.DecryptString(key, AppConstants.EncryptionKey);
 
             // encrypt key and license details
             var detailsToEncryptModel = new LicenseKeyGeneratorModel(decryptedKey, numberOfConnections, expiryDate);
             string detailsToEncrypt = JsonSerializer.Serialize(detailsToEncryptModel);
-            var encryptedKey = EncryptionService.EncryptString(detailsToEncrypt, encryptionKey);
+            var encryptedKey = EncryptionService.EncryptString(detailsToEncrypt, AppConstants.EncryptionKey);
             return Convert.ToBase64String(Encoding.UTF8.GetBytes(encryptedKey));
         }
 
